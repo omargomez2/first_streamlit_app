e4823d748d9e92f67c79b015303da3ee4d50010c
Omar S. Gómez <omargomez2@gmail.com>
2022-12-30 12:19:55 -0500

    Update streamlit_app.py
    Added pandas library, with a data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
+#---------------------------------
+# Author: OG
+# Description: Python script related to the hands on snowflake series workshop
+#---------------
+
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My new healthy diner spot')
 
@@ -9,3 +17,5 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast with blue cheese')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+straeamlit.dataframe(my_fruit_list)

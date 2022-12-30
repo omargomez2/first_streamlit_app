40ce87711b046b429868e70cefe58a18baee74d8
Omar S. Gómez <omargomez2@gmail.com>
2022-12-30 12:32:34 -0500

    Update streamlit_app.py
    Added a multiselection combo box

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('My new healthy diner spot')
 
@@ -17,5 +18,10 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast with blue cheese')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 
 streamlit.dataframe(my_fruit_list)

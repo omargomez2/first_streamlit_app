3f15638ee9034d8541c0841c9159074d4bdbc64a
Omar S. Gómez <omargomez2@gmail.com>
2023-01-01 11:41:08 -0500

    Update streamlit_app.py
    Show the json response in a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!!!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)

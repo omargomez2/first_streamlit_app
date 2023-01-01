3b46fc0a0f146f2c0c508b2b6375e171a178ecdd
Omar S. Gómez <omargomez2@gmail.com>
2023-01-01 11:43:57 -0500

    Update streamlit_app.py
    added var fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,9 @@ import streamlit
 import pandas
 import requests
 
+fruit = "kiwi"
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit)
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -28,7 +31,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!!!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())

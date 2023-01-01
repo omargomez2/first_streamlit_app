9547167fb20b21a310d6a49c2d5e230134924bec
Omar S. Gómez <omargomez2@gmail.com>
2023-01-01 11:37:32 -0500

    Update streamlit_app.py
    Return JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,4 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!!!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())

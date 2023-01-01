3cff744479c02c2aa464e2693cd51e8577910321
Omar S. Gómez <omargomez2@gmail.com>
2023-01-01 11:44:55 -0500

    Update streamlit_app.py
    fixed bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!!!')
- fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())

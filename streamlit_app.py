83612878f3a3032e233b7c7fb360a403f0778b14
Omar S. Gómez <omargomez2@gmail.com>
2023-01-01 11:46:26 -0500

    Update streamlit_app.py
    Added input text

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!!!')
 #streamlit.text(fruityvice_response.json())
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)

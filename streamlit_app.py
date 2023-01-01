f41702af3b2e8efc9bfc481a52b169dfba21b96f
Omar S. Gómez <omargomez2@gmail.com>
2023-01-01 14:09:05 -0500

    Update streamlit_app.py
    add other input text

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+

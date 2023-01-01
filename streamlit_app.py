bb1b81dbb20e3fbfe78451ea02fe23957b46e389
Omar S. Gómez <omargomez2@gmail.com>
2023-01-01 11:36:07 -0500

    Update streamlit_app.py
    Api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@
 
 import streamlit
 import pandas
+import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -25,3 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header('Fruityvice Fruit Advice!!!')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)

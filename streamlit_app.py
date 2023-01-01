068fdc80e7d2b62e6e180e8635cef8b8853e845a
Omar S. Gómez <omargomez2@gmail.com>
2023-01-01 11:54:30 -0500

    Update streamlit_app.py
    added var to hold the user selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,7 @@ import streamlit
 import pandas
 import requests
 
-fruit = "kiwi"
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit)
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -35,6 +34,8 @@ streamlit.header('Fruityvice Fruit Advice!!!')
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?

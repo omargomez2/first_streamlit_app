89bdbd3c8b4392430b7ed30b9fb7e8bc2eeeeeb1
Omar S. Gómez <omargomez2@gmail.com>
2023-01-11 12:40:10 -0500

    Update streamlit_app.py
    up to pg

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,9 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError
+import psycopg2
 
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -58,31 +59,37 @@ except URLError as e:
 
 #Snowflake-related functions
 def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from fruit_load_list")
+  with conn.cursor() as my_cur:
+    conn.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 
 
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+  
 streamlit.header("View Our Fruit List - Add Your Favorites!")
 # Add a button to load the fruit
 if streamlit.button('Get Fruit List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  conn = init_connection()
+  #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
-  my_cnx.close()
+  conn.close()
+  #my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 
 def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
+  with conn.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list (fruit_name) values ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
 
   
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  conn = init_connection()
+  #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
-  my_cnx.close()
+  conn.close()
   streamlit.text(back_from_function)
-  
-  #streamlit.write('Thanks for adding ', add_my_fruit)
+  streamlit.write('Thanks for adding ', add_my_fruit)
